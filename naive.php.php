65d9fedcecd9deaa12527221bbb299071f73e4b0
dheekshithadev26 <dheekshi.dev269@gmail.com>
2019-03-23 09:49:00 +0530

    Add files via upload

---
+++ naive.php.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+      <title>form1</title>
+      <center>
+      <h1>Naive bayes pie-chart</h1>
+      <img src="img/capture.jpeg" height="200px" width="300px"><br><br>
+      <h1>pivot Table</h1>
+      <img src="img/capture01.jpeg" height="200px" width="300px"><br><br>
+      <form action="naive1.php">
+            <button type="submit" class="btn btn-primary">Next</button>
+         </form>
+      </head>
+   </center>
+      </html>
